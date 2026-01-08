5b0e69c17d92b130b60b65afd7a6a288c2e3268f
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-08 16:02:16 +0200

    auth fix

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,7 +3,33 @@ import { SessionsCollection } from '../models/session.js';
 import { UsersCollection } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const authHeader = req.headers.authorization; //TODO check headers
+  const { accessToken } = req.cookies;
+
+  if (!accessToken) return next(createHttpError(401, 'Missing access token'));
+
+  const session = await SessionsCollection.findOne({ accessToken });
+  if (!session) return next(createHttpError(401, 'Session not found'));
+
+  if (session.accessTokenValidUntil < new Date())
+    return next(createHttpError(401, 'Access token expired'));
+
+  const user = await UsersCollection.findById(session.userId);
+  if (!user)
+    return next(
+      createHttpError(401, 'No session associated with this token found'),
+    );
+
+  req.user = user;
+
+  next();
+};
+
+/* import createHttpError from 'http-errors';
+import { SessionsCollection } from '../models/session.js';
+import { UsersCollection } from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  const authHeader = req.cookies; //TODO check headers
 
   if (!authHeader)
     throw createHttpError(401, 'Authorization token is required!');
@@ -34,3 +60,4 @@ export const authenticate = async (req, res, next) => {
 
   next();
 };
+ */
--- src/services/weeks.js
+++ src/services/weeks.js
@@ -6,6 +6,7 @@ import {
 } from '../utils/calculateDates.js';
 import { MomStatesCollection } from '../models/momStates.js';
 import { validateWeekNumber } from '../utils/validateWeekNumber.js';
+import { FIRST_WEEK, PRESUMABLE_DAYS } from '../constants/times.js';
 
 export const getDemoInfo = async () => {
   const { weekNumber, daysLeftToBirth } = calcDemoWeek();
@@ -20,22 +21,25 @@ export const getDemoInfo = async () => {
 };
 
 export const getWeeksService = async (user) => {
-  // if (!user) throw createHttpError(401, 'No user found, auth problem'); //TODO auth check
+  if (!user) throw createHttpError(401, 'No user found, auth problem'); //TODO auth check
 
-  const { weekNumber, daysLeftToBirth } = calcCurrentWeekFromUser(user);
+  let { weekNumber, daysLeftToBirth } = calcCurrentWeekFromUser(user);
+
+  //if no dueDate
+  if (!weekNumber || !daysLeftToBirth)
+    [weekNumber, daysLeftToBirth] = [FIRST_WEEK, PRESUMABLE_DAYS];
 
   const babyState = await BabyStatesCollection.findOne({ weekNumber });
   const momState = await MomStatesCollection.findOne({ weekNumber });
 
-  if (!babyState || !momState) {
-    throw createHttpError(404, 'Data not found for this week');
-  }
+  if (!babyState || !momState)
+    throw createHttpError(404, `No info found for week ${weekNumber}`);
 
   return { weekNumber, daysLeftToBirth, babyState, momState };
 };
 
 export const getBabyService = async (weekNum, user) => {
-  // if (!user) throw createHttpError(401, 'No user found, auth problem'); //TODO auth check
+  if (!user) throw createHttpError(401, 'No user found, auth problem'); //TODO auth check
 
   const weekNumber = weekNum
     ? validateWeekNumber(weekNum)
