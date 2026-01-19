2cc8b0b3eebae6c9f11f60e979cd9ac83646f78a
Anna Polisciuc <annap00775@gmail.com>
2026-01-19 21:38:36 +0200

    fix auth

--- src/routes/authGoogle.js
+++ src/routes/authGoogle.js
@@ -12,8 +12,13 @@ const client = new OAuth2Client(
   `${process.env.BACKEND_URL}/api/auth/google/callback`
 );
 
+console.log('CLIENT_ID', process.env.GOOGLE_CLIENT_ID);
+console.log('BACKEND_URL', process.env.BACKEND_URL);
+console.log('REDIRECT URI', client.redirectUri);
+
 
 router.get('/auth/google', (req, res) => {
+  
   const url = client.generateAuthUrl({
     access_type: 'offline',
     scope: ['email', 'profile'],
@@ -34,15 +39,15 @@ router.get('/auth/google/callback', async (req, res, next) => {
     const { tokens } = await client.getToken(code);
     client.setCredentials(tokens);
 
-    if (!tokens.id_token) {
-      return res.status(400).json({ message: 'id_token is missing' });
-    }
+    // if (!tokens.id_token) {
+    //   return res.status(400).json({ message: 'id_token is missing' });
+    // }
 
     const session = await googleAuthService(tokens.id_token);
 
     setSessionCookies(res, session);
 
-    res.redirect(`${process.env.FRONTEND_URL}/dashboard`);
+    res.redirect(`${process.env.FRONTEND_URL}/`);
   } catch (error) {
     next(error);
   }
